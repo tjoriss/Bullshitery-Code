d6db56d7bca4c0cc8931a144b40a13b6c7a7a625
Simon <spammer1999@web.de>
2019-02-21 19:36:01 +0100

    select random between picture and video

--- Random 9gag image/main.py
+++ Random 9gag image/main.py
@@ -4,11 +4,14 @@ __author__ = 'EmilioK'
 import sys
 import re
 import requests
+import random
 
 def main(argv):
-	for each in range(0, 100):
-		get_image()
-
+	for each in range(0, 10):
+		if random.randint(0,1) == 0:
+			get_image()
+		else:
+			get_video()
 
 def get_image():
 	response = requests.get('http://www.9gag.com/random')
@@ -19,8 +22,21 @@ def get_image():
 		print (result[0])
 	
 	except AttributeError:
-		print("Error Catched")
+		print("Picturerror Catched")
 		pass
+	
+def get_video():
+	response = requests.get('http://www.9gag.com/random')
+	html = response.text
+	
+	try:
+		result2 = re.findall(r'(?<=vp9Url\":\").*?.webm',html)
+		
+		each = result2[0].replace('\/','/')
+		print(each)
+	except AttributeError:
+		print ("VideoError Catched")
+
 
 if __name__ == '__main__':
 	main(sys.argv)
