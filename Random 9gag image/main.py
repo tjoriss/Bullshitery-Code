b88bebc35cc9a8f2403b70bad606130ee316453f
Simon <spammer1999@web.de>
2019-02-21 18:06:55 +0100

    First Rework, pictures Only

--- Random 9gag image/main.py
+++ Random 9gag image/main.py
@@ -1,33 +1,26 @@
 # Get 9gag image.
-
 __author__ = 'EmilioK'
 
 import sys
-import urllib2
 import re
 import requests
 
-
 def main(argv):
-	for i in range(0, 100):
+	for each in range(0, 100):
 		get_image()
 
 
 def get_image():
 	response = requests.get('http://www.9gag.com/random')
-
-	response = urllib2.urlopen(response.url)
-	html = response.read()
-
+	html = response.text
+	
 	try:
-		if 'type="video/webm"' in html:
-			result = re.search('<source src="(.*)" type="video/webm"', html)
-			print result.group(1)
-		else:
-			result = re.search('<img class="badge-item-img" src="(.*)" alt', html)
-			print result.group(1)
+		result = re.findall(r'(?<=\<link rel=\"image_src\" href=\").*.jpg', html)
+		print (result[0])
 	except AttributeError:
+		print("Error Catched")
 		pass
 
 if __name__ == '__main__':
 	main(sys.argv)
+	
